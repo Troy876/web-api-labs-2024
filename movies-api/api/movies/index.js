42f7583f7d49ccb20f5f50c64728d39153183a72
Troy <20098362@mail.wit.ie>
2024-11-27 17:00:29 +0000

    Authentication lab - exercise (genres)

--- movies-api/api/movies/index.js
+++ movies-api/api/movies/index.js
@@ -4,6 +4,7 @@ import express from 'express';
 import {
     getUpcomingMovies
 } from '../tmdb-api';
+import { getGenre } from '../tmdb-api';
 
 const router = express.Router();
 
@@ -42,6 +43,11 @@ router.get('/:id', asyncHandler(async (req, res) => {
 router.get('/tmdb/upcoming', asyncHandler(async (req, res) => {
     const upcomingMovies = await getUpcomingMovies();
     res.status(200).json(upcomingMovies);
+}));
+
+router.get('/tmdb/genres', asyncHandler(async (req, res) => {
+    const genres = await getGenre();
+    res.status(200).json(genres);
 }));
 
 export default router;
